52570f9fc5842f714abd88d9ab5a087b11bee98a
thorstenalpers <thorstenalpers@foo.de>
2023-08-15 15:54:57 +0200

    Add sql view for courses

---
+++ src/MarketData/MarketData.Infratructure/Migrations/20230815135329_CreateRecordsView.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketData.Infratructure.Migrations;
+
+/// <inheritdoc />
+public partial class CreateRecordsView : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+                DROP VIEW IF EXISTS ""TradeX"".""AssetRecordsView"";
+                CREATE OR REPLACE VIEW ""TradeX"".""AssetRecordsView"" AS
+                SELECT t1.""Id"", t1.""Date"", t1.""Value""::numeric, t1.""AssetId"", t1.""AssetMetricId"", t2.""Symbol"", t2.""Name""
+                FROM ""TradeX"".""AssetRecords"" t1
+                LEFT JOIN 
+                    (SELECT * FROM ""TradeX"".""Assets"") t2
+                ON t1.""AssetId"" = t2.""Id"";
+            ");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+
+    }
+}
